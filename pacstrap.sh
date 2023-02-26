21874e1dac62678ecfcba6c906cee8dc8b5a3e87
hcarter1112 <hcarter1112@gmail.com>
2023-02-26 18:37:04 -0500

    more fixes

--- pacstrap.sh
+++ pacstrap.sh
@@ -3,5 +3,6 @@
 pacstrap /mnt base base-devel linux linux-lts linux-headers linux-lts-headers man-db vim networkmanager connman sudo btrfs-progs grub efibootmgr intel-ucode &&
 genfstab -U /mnt > /mnt/etc/fstab &&
 cp -r /root/ARCH/ /mnt/ &&
-arch-chroot /mnt 
+arch-chroot /mnt &&
+cd ARCH
 
