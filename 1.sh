cbc2bfa20e8811114fa6728b9a933eee872e9223
hcarter1112 <hcarter1112@gmail.com>
2023-02-25 23:19:03 -0500

    separated install scripts

--- 1.sh
+++ 1.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 #
 
-./pacstrap.sh &&
-./user-setup.sh &&
-./install-pkgs-native.sh &&
-./enable-services.sh &&
-./switch-ralldi &&
-./aur-setup.sh &&
-./install-pkgs-aur.sh &&
-./install-lvim.sh &&
-./chezmoi.sh &&
+./pacstrap.sh 
--- pacstrap.sh
+++ pacstrap.sh
@@ -3,5 +3,6 @@
 cp -r pkg/ /mnt/var/cache/pacman/ &&
 pacstrap /mnt < pkglist-pacstrap.txt &&
 genfstab -U /mnt > /mnt/etc/fstab &&
+cp . /mnt/ &&
 arch-chroot /mnt 
 
