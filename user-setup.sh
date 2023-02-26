803643be8b984def959603f2db707584703a334f
hcarter1112 <hcarter1112@gmail.com>
2023-02-26 00:48:29 -0500

    fix to v4l2.service

--- user-setup.sh
+++ user-setup.sh
@@ -20,6 +20,7 @@ cd / &&
 cp sudoers /etc/ &&
 cp mkinitcpio.conf /etc/ &&
 cp grub /etc/default/ &&
+cp v4l2.service /etc/systemd/system/ &&
 mkinitcpio -P &&
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARX &&
 grub-mkconfig -o /boot/grub/grub.cfg
