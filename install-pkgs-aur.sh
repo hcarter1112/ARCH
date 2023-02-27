4dd14167cd2d722140e766b19fbede5e4876059c
hcarter1112 <hcarter1112@gmail.com>
2023-02-26 19:02:48 -0500

    add cargo install to aur pkg install.

--- install-pkgs-aur.sh
+++ install-pkgs-aur.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #
+rustup default stable &&
 yay -S betterlockscreen librewolf-bin zoom tty-clock-git ferdium gruvbox-dark-gtk gruvbox-dark-icons-gtk gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git gtk-theme-arc-gruvbox-git ncspot-git xcursor-comix-opaque zhumu
