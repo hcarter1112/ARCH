eb31972d5f0b03c9a94c7bd553ea13aa88827531
hcarter1112 <hcarter1112@gmail.com>
2023-02-27 09:21:33 -0500

    removed redundent intel ucode install from pactrap script

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacstrap /mnt base base-devel linux linux-lts linux-headers linux-lts-headers man-db vim networkmanager connman sudo btrfs-progs grub efibootmgr intel-ucode &&
+pacstrap /mnt base base-devel linux linux-lts linux-headers linux-lts-headers man-db vim networkmanager connman sudo btrfs-progs grub efibootmgr &&
 genfstab -U /mnt > /mnt/etc/fstab &&
 cp -r /root/ARCH/ /mnt/ &&
 arch-chroot /mnt &&
