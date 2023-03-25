e4a2ee6551254fb095f2a7dc6030ba07c2c551df
hcarter1112 <hcarter1112@gmail.com>
2023-03-25 09:58:06 -0500

    path fix in aur-setup.sh

--- aur-setup.sh
+++ aur-setup.sh
@@ -4,5 +4,5 @@ cd /home/ralldi/.gitstuff/ &&
 	if [[ ! -d ~/.gitstuff/pikaur-git/ ]]; then
 		git clone https://aur.archlinux.org/pikaur-git.git
 	fi
-cd ~/.gitstuff/pikaur-git/ &&
+cd /home/ralldi/.gitstuff/pikaur-git/ &&
 	makepkg -si
